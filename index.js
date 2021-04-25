7f743aad0662c78018f74a3013b810e176b7340b
Hamid Kamyab <hamidkamyab1377@gmail.com>
2021-04-26 02:37:23 +0430

    Final JS commits

--- index.js
+++ index.js
@@ -16,29 +16,27 @@ function callAPI(userInput){
   });
 }
 
-
-
 $('.btn').click(function(){
   var a = $("input[name='userInput']").val();
   if (a.length === 0 ) {
     $(".label")
     .html("Please Add a link");
     $("#input")
-    .attr("value","Shorten a link here...")
-    .addClass('red')
-    .css({"border": "1px solid red"})
-    .click(function(){
-      $('.label').html("");
-      $("#input")
-      .attr("value","")
-      .removeClass('red')
-      .css({'border': 'none'});
+      .attr("value","Shorten a link here...")
+      .addClass('red')
+      .css({"border": "1px solid red"})
+      .click(function(){
+        $('.label')
+          .html("");
+        $("#input")
+          .attr("value","")
+          .removeClass('red')
+          .css({'border': 'none'});
     });
     //changing PlaceHolder color on Error is going to FUCKED ME UP 01:15 AM
     //Solution : there were no solution so i decide to do this with
     //input value attribute 02:07 AM
   }else{
-
     callAPI(a);
   }
 
