431f7b4f3e916fc442a99ff1ba1f2e134860a521
Hamid Kamyab <hamidkamyab1377@gmail.com>
2021-04-26 18:20:51 +0430

    Working On Shorter Links Detail

--- index.js
+++ index.js
@@ -4,28 +4,76 @@ const historyAndDetails= [];
 
 function callAPI(userInput){
   url = 'https://api.shrtco.de/v2/shorten?url='+ userInput;
-  $.get( url , function (data , result) {
-    let shortLink = data.result.full_short_link;
-    console.log(shortLink);
-    let detail = {
-      urlUserSend: url ,
-      urlShortened: shortLink
+  fetch(url)
+  .then( function (response) {
+
+    if ( response.status >= 400 && response.status<=499){
+      alert ("ERROR! BAD INPUT ENTER A VALID URL");
+      return null;
+
+    } else if (response.status >= 500 && response.status<=599){
+      alert ("ERROR! ON SERVER PLEASE TRY AGIN LATER");
+
+    }else {
+      return response.json();
+
+    }
+  })
+  .then(function (res){
+    let shortLink = res.result.full_short_link2;
+
+    let linkDetails = {
+      urlUserEntered: userInput,
+      urlShorted: shortLink
     };
-    historyAndDetails.push(detail);
-    console.log(historyAndDetails);
+    historyAndDetails.push(linkDetails);
+    if (historyAndDetails.length > 3) {
+      historyAndDetails.shift();
+    }
+    for ( let i = historyAndDetails.length-1; i > historyAndDetails.length - 4 ; i--){
+      console.log(historyAndDetails);
+      $("#middle-section .container")
+        .append("<div class='contain-short-link'>"+
+         historyAndDetails[i].urlUserEntered+
+        //"<div class='link-and-copy'>"+
+        "<a id='link' href='"+historyAndDetails[i].urlShorted+"'>"+historyAndDetails[i].urlShorted+"</a>"+
+        "<button class='btn'>Copy</button>"+//"</div>"+
+        "</div>"
+      ).fadeIn(900);
+    }
+
+    $(".contain-short-link .btn").click(function(){
+      (".contain-short-link .btn").fadeOut(400);
+      copy("#link");
+    });
+
   });
 }
 
+function copy (a) {
+    let copyText = $(a);
+    copyText.select();
+    document.execCommand("copy");
+}
+
+
+
 $('.btn').click(function(){
-  var a = $("input[name='userInput']").val();
-  if (a.length === 0 ) {
+  $("#middle-section .container")
+  .fadeOut(600 , function(){
+    $("#middle-section .container").html("");
+  });
+  let a = $("#input").val();
+
+  if (a.length === 0 ) { // if the Input Were Empty
     $(".label")
     .html("Please Add a link");
     $("#input")
       .attr("value","Shorten a link here...")
       .addClass('red')
       .css({"border": "1px solid red"})
-      .click(function(){
+
+      .click(function(){ // reseting the Empty Notification by clicking on input box
         $('.label')
           .html("");
         $("#input")
@@ -38,6 +86,9 @@ $('.btn').click(function(){
     //input value attribute 02:07 AM
   }else{
     callAPI(a);
+    $(".contain-short-link .btn").click(function(){
+      copy("#link");
+    });
   }
 
 });
